e7bffbdd37856f3e19da4d68f0f612048aff4f59
Rectmoon <15692087320@163.com>
2020-02-12 07:24:28 +0800

    first commit

--- config-overrides.js
+++ config-overrides.js
@@ -1,27 +1,94 @@
+const fs = require('fs')
 const path = require('path')
-const { override, addWebpackAlias, addBabelPlugins } = require('customize-cra')
+const globby = require('globby')
+const { paths } = require('react-app-rewired')
+const { override } = require('customize-cra')
+
 const resolve = dir => path.resolve(__dirname, dir)
+const defaultTemplatePath = resolve('public/index.html')
+
+function multiEntryPlugin(entries) {
+  return function(config, env) {
+    if (!entries || !entries.length) return config
+    const defaultEntryName = 'main'
+    const defaultEntryHTMLPlugin = config.plugins.filter(plugin => plugin.constructor.name === 'HtmlWebpackPlugin')[0]
+    defaultEntryHTMLPlugin.options.chunks = [defaultEntryName]
+
+    config.entry = entries.reduce(
+      (res, next) => {
+        const { name, entry, template, filename } = next
+        const chunks = env === 'production' ? ['manifest', 'vendor', name] : [name]
+        config.plugins.push(
+          new defaultEntryHTMLPlugin.constructor(
+            Object.assign({}, defaultEntryHTMLPlugin.options, {
+              filename,
+              template,
+              chunks
+            })
+          )
+        )
+        res[name] = entry
+        return res
+      },
+      { [defaultEntryName]: config.entry }
+    )
+
+    if (env === 'development') {
+      config.output.filename = 'static/js/[name].js'
+    } else {
+      config.output.filename = 'static/js/[name].[chunkhash:6].js'
+      config.output.chunkFilename = 'static/js/[name].[chunkhash:6].js'
+
+      config.optimization = {
+        moduleIds: 'hashed',
+
+        runtimeChunk: {
+          name: 'manifest'
+        },
+
+        splitChunks: {
+          cacheGroups: {
+            vendor: {
+              test: /[\\/]node_modules[\\/](react|react-dom|echarts-for-react)[\\/]/,
+              name: 'vendor',
+              chunks: 'all'
+            },
+
+            styles: {
+              name: 'styles',
+              test: /\.css|less$/,
+              chunks: 'all',
+              enforce: true
+            }
+          }
+        }
+      }
+    }
+
+    return config
+  }
+}
+
+const entries = globby.sync([paths.appSrc + '/*/index.js'], { cwd: process.cwd() }).reduce((res, entry) => {
+  const name = path
+    .relative(paths.appSrc, entry)
+    .split(path.sep)
+    .slice(0, -1)
+    .join('/')
+  const tpName = entry.replace('.js', '.html')
+  const template = fs.existsSync(tpName) ? tpName : defaultTemplatePath
+  const filename = `${name}.html`
+
+  res.push({
+    name,
+    entry,
+    template,
+    filename
+  })
+
+  return res
+}, [])
 
 module.exports = {
-  webpack: override(
-    override(
-      addWebpackAlias({
-        '@': resolve('src')
-      })
-    ),
-    ...addBabelPlugins(
-      ['react-loadable/babel'],
-      [
-        '@babel/plugin-proposal-decorators',
-        {
-          legacy: true
-        }
-      ]
-    )
-  ),
-  paths: paths => {
-    paths.appBuild = path.resolve(__dirname, '../server/public')
-    paths.servedPath = '/public/'
-    return paths
-  }
+  webpack: override(multiEntryPlugin(entries))
 }
